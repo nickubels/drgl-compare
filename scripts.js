8b2ed3f9b3cb2556ea7ed5028cd758cefaf1c7e5
Nick Ubels <nickubels@gmail.com>
2020-12-29 22:00:44 +0100

    Added some comments and meta tags

--- scripts.js
+++ scripts.js
@@ -1,34 +1,44 @@
+// Attributions for bottom right corner
+var attributions = "Ontwikkeling: <a href='http://nickubels.nl'>Nick Ubels</a> - <a href='https://github.com/nickubels/drgl-compare'>GitHub</a> - Lijnnetkaarten: &copy; <a href='https://cartostudio.nl'>Carto Studio</a> / <a href='https://qbuzz.nl/gd'>Qbuzz</a>";
+
+// Layer for left side (old map)
 var layer1 = new ol.layer.Tile({
     source: new ol.source.XYZ({
-        attributions: "Test",
+        attributions: attributions,
         url: './2020/{z}/{y}/{x}.png',
         maxZoom: 6,
     })
 });
 
+// Layer for the right side (new map)
 var layer2 = new ol.layer.Tile({
     source: new ol.source.XYZ({
-        attributions: "Test",
+        attributions: attributions,
         url: './2021/{z}/{y}/{x}.png',
         maxZoom: 6,
     })
 });
 
+// Setting up the OpenLayers map
 var map =  new ol.Map({
     target: 'map',
     layers: [layer1, layer2],
     view: new ol.View({
         center: [-10.7, 6.5],
-        zoom: 5
+        zoom: 4,
+        maxZoom: 6 // Limit the zoom capability as there are only 6 levels
     })
 });
 
+// Make sure we can access the swipe element and the divider
 var swipe = document.getElementById('swipe');
 var divider = document.getElementById('divider');
 
+// Rendering right side
 layer2.on('prerender', function (event) {
   var ctx = event.context;
   var mapSize = map.getSize();
+  // Calculate offset for the divider
   var offset = (0.5 - swipe.value) * (53); // size of thumb + border size
   var width = mapSize[0] * swipe.value + offset;
   divider.style.left = width + 'px';
@@ -52,11 +62,11 @@ layer2.on('postrender', function (event) {
   ctx.restore();
 });
 
+// Event listener for changes in slider
 swipe.addEventListener(
   'input',
   function () {
     map.render();
-    console.log(swipe.value)
   },
   false
 );
